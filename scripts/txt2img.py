08f5a7ba68d14441e5cbae2d1e222799f3957aa7
Wonderofyou <130823525+Wonderofyou@users.noreply.github.com>
2025-04-14 09:52:58 +0700

    Update txt2img.py

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -105,7 +105,14 @@ if __name__ == "__main__":
 
 
     config = OmegaConf.load("configs/latent-diffusion/txt2img-1p4B-eval.yaml")  # TODO: Optionally download from same location as ckpt and chnage this logic
+    print("==============================")
+    print(config)
+    print("==============================")
+    
     model = load_model_from_config(config, "models/ldm/text2img-large/model.ckpt")  # TODO: check path
+    print("==============================")
+    print(model)
+    print("==============================")
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model = model.to(device)
